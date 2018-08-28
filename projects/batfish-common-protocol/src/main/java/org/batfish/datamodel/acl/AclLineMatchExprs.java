087d3ff6d72ad26e7fe0a8555f037f31c6c2a0fd
Matt Brown <matt@intentionet.com>
2018-08-28 11:33:14 -0700

    smart And/Or AclLineMatchExpr constructors

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/acl/AclLineMatchExprs.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/acl/AclLineMatchExprs.java
@@ -1,14 +1,18 @@
 package org.batfish.datamodel.acl;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Ordering;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
+import java.util.SortedSet;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpSpace;
 import org.batfish.datamodel.Prefix;
 
 public final class AclLineMatchExprs {
+
   private AclLineMatchExprs() {}
 
   public static final FalseExpr FALSE = FalseExpr.INSTANCE;
@@ -18,11 +22,19 @@ public final class AclLineMatchExprs {
 
   public static final TrueExpr TRUE = TrueExpr.INSTANCE;
 
-  public static AndMatchExpr and(AclLineMatchExpr... exprs) {
-    return and(Arrays.asList(exprs));
+  public static AclLineMatchExpr and(AclLineMatchExpr... exprs) {
+    return and(
+        Arrays.stream(exprs)
+            .collect(ImmutableSortedSet.toImmutableSortedSet(Comparator.naturalOrder())));
   }
 
-  public static AndMatchExpr and(List<AclLineMatchExpr> exprs) {
+  public static AclLineMatchExpr and(SortedSet<AclLineMatchExpr> exprs) {
+    if (exprs.isEmpty()) {
+      return TrueExpr.INSTANCE;
+    }
+    if (exprs.size() == 1) {
+      return exprs.first();
+    }
     return new AndMatchExpr(exprs);
   }
 
@@ -54,11 +66,18 @@ public final class AclLineMatchExprs {
     return new NotMatchExpr(expr);
   }
 
-  public static OrMatchExpr or(AclLineMatchExpr... exprs) {
-    return or(Arrays.asList(exprs));
+  public static AclLineMatchExpr or(AclLineMatchExpr... exprs) {
+    return or(
+        Arrays.stream(exprs).collect(ImmutableSortedSet.toImmutableSortedSet(Ordering.natural())));
   }
 
-  public static OrMatchExpr or(List<AclLineMatchExpr> exprs) {
+  public static AclLineMatchExpr or(SortedSet<AclLineMatchExpr> exprs) {
+    if (exprs.isEmpty()) {
+      return FalseExpr.INSTANCE;
+    }
+    if (exprs.size() == 1) {
+      return exprs.first();
+    }
     return new OrMatchExpr(exprs);
   }
 
