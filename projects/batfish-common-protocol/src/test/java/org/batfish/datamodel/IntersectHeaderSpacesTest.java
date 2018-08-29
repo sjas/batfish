b7f96b2d5fd0e7f587f07d77640b9566c7d07d48
Matt Brown <matt@intentionet.com>
2018-08-29 12:51:59 -0700

    Handle negated HeaderSpaceMatchExprs in AclExplanation

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/IntersectHeaderSpacesTest.java
@@ -0,0 +1,14 @@
+package org.batfish.datamodel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntersectHeaderSpacesTest {
+    @Test
+    public void testIntersect() {
+        HeaderSpace h1 = HeaderSpace.builder().build();
+        HeaderSpace h2 = HeaderSpace.builder().build();
+    }
+
+}
